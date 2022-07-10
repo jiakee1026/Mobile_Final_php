ba769e43853aeafd9f5181dfc9c1b5d609e54c59
JIA KEE <73268400+jiakee1026@users.noreply.github.com>
2022-07-10 17:31:42 +0800

    final

---
+++ Mobile_Final/php/payment.php
@@ -0,0 +1,40 @@
+<?php
+$email = $_GET['email'];
+$mobile = $_GET['mobile']; 
+$name = $_GET['name']; 
+$amount = $_GET['amount']; 
+
+
+$api_key = '';
+$collection_id = '';
+ 
+$host = '';
+
+
+$data = array(
+          'collection_id' => $collection_id,
+          'email' => $email,
+          'mobile' => $mobile,
+          'name' => $name,
+          'amount' => ($amount + 1) * 100,
+		  'description' => 'Payment for order by '.$name,
+          'callback_url' => "",
+          'redirect_url' => "email=$email &mobile=$mobile&amount=$amount&name=$name"
+);
+
+
+$process = curl_init($host );
+curl_setopt($process, CURLOPT_HEADER, 0);
+curl_setopt($process, CURLOPT_USERPWD, $api_key . ":");
+curl_setopt($process, CURLOPT_TIMEOUT, 30);
+curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($process, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($process, CURLOPT_POSTFIELDS, http_build_query($data) ); 
+
+$return = curl_exec($process);
+curl_close($process);
+
+$bill = json_decode($return, true);
+header("Location: {$bill['url']}");
+?>
